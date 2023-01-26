658aa0992a025325b8fbbf1ffd041b852a22c6e3
Isabela Costa <isa-hcosta@hotmail.com>
2023-01-26 19:28:23 -0300

    adiciona user model

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+          },
+          displayName: {
+            type: DataTypes.STRING,
+          },
+          email: {
+            type: DataTypes.STRING,
+            unique: true
+          },
+          password: {
+            type: DataTypes.STRING,
+          },
+          image: {
+            type: DataTypes.STRING
+          },
+    }, {
+        underscored: true,
+        tableName: 'users',
+    });
+
+    return User;
+}
