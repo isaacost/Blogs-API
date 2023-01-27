8774a7d92ab5f96bbb76ffe205404e5c5189b948
Isabela Costa <isa-hcosta@hotmail.com>
2023-01-27 18:47:44 -0300

    adiciona delete do post

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -10,5 +10,6 @@ router.post('/post', validateToken, validateCreatePost, postController.create);
 router.get('/post', validateToken, postController.findAll);
 router.get('/post/:id', validateToken, postController.findById);
 router.put('/post/:id', validateToken, updatePost, postController.update);
+router.delete('/post/:id', validateToken, postController.remove);
 
 module.exports = router;
