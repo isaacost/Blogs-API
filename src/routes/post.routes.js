0abf383b442b9843065e8da3499ba5a33adedac7
Isabela Costa <isa-hcosta@hotmail.com>
2023-01-27 18:36:28 -0300

    adiciona update do post

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const { postController } = require('../controller');
 const { validateCreatePost } = require('../middleware/validateCreatePost');
 const { validateToken } = require('../middleware/validateToken');
+const { updatePost } = require('../middleware/updatePost');
 
 const router = Router();
 
 router.post('/post', validateToken, validateCreatePost, postController.create);
 router.get('/post', validateToken, postController.findAll);
 router.get('/post/:id', validateToken, postController.findById);
+router.put('/post/:id', validateToken, updatePost, postController.update);
 
 module.exports = router;
