8f71cd073386530a819ca3735755630ed808f46a
Isabela Costa <isa-hcosta@hotmail.com>
2023-01-27 16:25:26 -0300

    adiciona o get do categories

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -5,5 +5,6 @@ const { validateToken } = require('../middleware/validateToken');
 const router = Router();
 
 router.post('/categories', validateToken, categoryController.create);
+router.get('/categories', validateToken, categoryController.findAll);
 
 module.exports = router;
